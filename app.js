d2b30b515198873b83af6f889bbdbf7e7bff579c
delhipv-cell <delhi.pv@digitalazadi.com>
2026-04-28 12:11:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
