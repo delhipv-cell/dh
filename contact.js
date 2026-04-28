95d4f6bce0dd5baefddd22bc6213f1e71b6f3f8f
delhipv-cell <delhi.pv@digitalazadi.com>
2026-04-28 11:55:55 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,16 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Contact Us</h1>
+      <p>Phone: +91 9205791900</p>
+      <p>Email: info@digitalhorizon.com</p>
+
+      <iframe
+        src="https://maps.google.com/maps?q=paschim%20vihar%20delhi&output=embed"
+        width="100%"
+        height="300"
+        title="map"
+      />
+    </div>
+  );
+}
